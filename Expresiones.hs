65697a560b88d3f3b30c5612138fcebac8a613ed
Christian <christianol_01@hotmail.com>
2017-03-09 14:28:08 -0400

    Divex

--- Expresiones.hs
+++ Expresiones.hs
@@ -15,6 +15,47 @@ anaFuncion f = do
     anaExpr (Out.ExpFcall f)
 
 anaExpr :: Out.Expr -> Context.ConMonad Context.State
+anaExpr (Out.Divex e1 e2 p) = do
+    anaExpr e1
+    st <- get
+    let et1 = topTable $ tablas st
+    case et1 of
+        Context.ExprTable Context.Boolean _ _ -> do 
+            throw $ Context.ContextError ("Cerca de la siguiente posicion" 
+                                            ++ (Out.printPos p)
+                                            ++ " en Operacion '/', se esperaba un Tipo Number y se encontro expresion Tipo Boolean en operando izquierdo")
+        Context.ExprTable Context.Number c n -> do
+            put $ modifyTable popTable st
+        _ -> do
+            error "Error interno, algo salio mal y no esta la tabla de la expresion"
+
+    anaExpr e2
+    st <- get
+    let et2 = topTable $ tablas st
+    case et2 of
+        Context.ExprTable Context.Boolean _ _ -> do 
+            throw $ Context.ContextError ("Cerca de la siguiente posicion" 
+                                            ++ (Out.printPos p)
+                                            ++ " en Operacion '/', se esperaba un Tipo Number y se encontro expresion Tipo Boolean en operando derecho")
+        Context.ExprTable Context.Number Context.Dynamic n -> do
+            let st = modifyTable popTable st
+            return ( modifyTable (pushTable (Context.ExprTable Context.Number Context.Dynamic n)) st )
+        Context.ExprTable Context.Number c n -> do
+            case et1 of
+                Context.ExprTable Context.Number Context.Dynamic n-> do
+                    let st = modifyTable popTable st
+                    return ( modifyTable (pushTable (Context.ExprTable Context.Number Context.Dynamic n)) st )
+                Context.ExprTable Context.Number c (Context.CNumber 0) -> do
+                    throw $ Context.ContextError ("Cerca de la siguiente posicion" 
+                                                                ++ (Out.printPos p)
+                                                                ++ " en Operacion '/', division entre 0")
+                Context.ExprTable Context.Number c (Context.CNumber n1) -> do
+                    let st = modifyTable popTable st
+                    return ( modifyTable (pushTable (Context.ExprTable Context.Number c (modifyDoubleValCalc (\x -> n1 / x) n))) st )
+
+        _ -> do
+            error "Error interno, algo salio mal y no esta la tabla de la expresion"
+
 anaExpr (Out.Modex e1 e2 p) = do
     anaExpr e1
     st <- get
