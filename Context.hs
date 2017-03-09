b7421910d97c4a8f46c5f9d28b4cfe5c63aad201
Pablo <pablodbc30@gmail.com>
2017-03-09 19:00:56 -0400

    Modificada la funcion findExpr de nuevo

--- Context.hs
+++ Context.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE DeriveDataTypeable #-}
 module Context where
 import qualified Lexer
 import qualified Stdout as Out
@@ -6,26 +5,18 @@ import qualified Grammar
 import Data.Map as M 
 import Control.Monad.RWS
 import Prelude as P
-import Control.Exception as E
-import Data.Typeable as T
 
-data ContextError = ContextError String
-    deriving (T.Typeable)
-
-instance E.Exception ContextError
-instance Show ContextError where
-    show (ContextError s) = "Error de Contexto: " ++ s
 
 data CompType = Dynamic | Constant deriving (Eq,Show,Ord)
 
 data Type     = Boolean | Number | Void deriving (Eq,Show,Ord)
 
-data ValCalc  = CBoolean Bool | CNumber Double | Nein deriving (Eq,Show,Ord)
+data ValCalc  = Bool | Double | Nein deriving (Eq,Show,Ord)
 
 
 data Tabla    = ExprTable {tipo :: Type, compType :: CompType, val :: ValCalc} |
                 FuncionTable {rtype :: Type}                                   |
-                SymTable {mapa :: Map String (Type,ValCalc), height :: Int}
+                SymTable {mapa :: Map String (Type,ValCalc)}
                 deriving (Eq,Show,Ord)
 
 data FunProto = FunProto {retype :: Type, args :: [Type], size :: Int} deriving (Eq,Show,Ord)
@@ -34,41 +25,12 @@ data FunHandler = FunHandler {id :: String, ret :: Bool} | None deriving (Eq,Sho
 
 data State = State {funcs :: Map String FunProto, tablas :: [Tabla], funDecl :: FunHandler, h :: Int} deriving (Eq,Show,Ord)
 
--- Monad que usaremos para hacer estas cosas. El primer tipo es arbitrario (Reader maneja el separador)
-type ConMonad = RWS String String State
+-- Monad que usaremos para hacer estas cosas. El primer tipo es arbitrario (No usaremos el reader)
+type ConMonad = RWS Bool String State
 
 initialState = State M.empty [] None 0
 
--- ValCalc Modifiers
-modifyBoolValCalc :: (Bool -> Bool) -> ValCalc -> ValCalc
-modifyBoolValCalc f (CBoolean b) = CBoolean (f b)
 
-modifyDoubleValCalc :: (Double -> Double) -> ValCalc -> ValCalc
-modifyDoubleValCalc f (CNumber n) = CNumber (f n)
-
--- Number Handlers
-numberConversionHandler :: (RealFrac a, Integral b) => a -> b
-numberConversionHandler = floor
-
-applyIntegerFun ::(Integral a, RealFrac b) => (a -> a -> a) -> b -> b -> b
-applyIntegerFun f x y = fromIntegral(f (numberConversionHandler x) (numberConversionHandler y))
-
-modex :: RealFrac a => a -> a -> a
-modex x y = x - (y * (fromIntegral $ truncate (x/y)))
-
-
--- Comparison Handler
-comparisonFunNum :: (Ord a, RealFrac a) => (a -> a -> Bool) -> a -> a -> ValCalc
-comparisonFunNum f x y = CBoolean (f x y)
-
-comparisonFunBool :: (Bool -> Bool -> Bool) -> Bool -> Bool -> ValCalc
-comparisonFunBool f x y = CBoolean (f x y)
-
-
-
-
-
--- State Handlers
 pushTable :: Tabla -> [Tabla] -> [Tabla]
 pushTable tabla tablas = tabla:tablas
 
@@ -85,15 +47,20 @@ modifyHeight :: (Int -> Int) -> State -> State
 modifyHeight f (State fs t fd h) = State fs t fd (f h)
 
 isSymTable :: Tabla -> Bool
-isSymTable (SymTable _ _) = True
+isSymTable (SymTable _) = True
 isSymTable _ = False
 
 onlySymTable :: [Tabla] -> [Tabla]
 onlySymTable = P.filter (isSymTable)
 
+
+isNothing :: Maybe a -> Bool
+isNothing Nothing = True
+isNothing _ = False
+
 findExpr :: String -> [Tabla] -> Maybe (Type,ValCalc)
 
 findExpr _ [] = Nothing
-findExpr s (x:xs) = case Nothing of True -> findExpr s xs
-								otherwise -> r
+findExpr s (x:xs) = case (isNothing r) of True -> findExpr s xs
+                                          otherwise -> r
                             where r = M.lookup s (mapa x)
