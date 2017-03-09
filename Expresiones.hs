d4a921c166747d07c47e96686892922600e29b3c
Christian <christianol_01@hotmail.com>
2017-03-09 03:37:42 -0400

    Some Expresions Working

--- Expresiones.hs
+++ Expresiones.hs
@@ -1,5 +1,5 @@
 module Expresiones where
-import qualified Lexer
+import qualified Lexer as Lexer
 import qualified Stdout as Out
 import qualified Grammar 
 import Data.Map as M 
@@ -7,10 +7,34 @@ import Control.Monad.RWS
 import Context as Context
 
 
-anaExpr :: Out.Expr -> Context.ConMonad Context.Tabla
+anaFuncion :: Out.Funcion -> Context.ConMonad Context.State
+-- Esto esta terriblemente mal pero es para compilar
+anaFuncion f = anaExpr (Out.ExpFcall f)
+
+anaExpr :: Out.Expr -> Context.ConMonad Context.State
+anaExpr (Out.ExpFalse _) = do
+    st <- get
+    return ( modifyTable (pushTable (Context.ExprTable Context.Boolean Context.Constant False)) st )
+
+anaExpr (Out.ExpFcall f) = do
+    anaFuncion f
+    st <- get
+    case topTable $ tablas st of
+        FuncionTable r -> do 
+            case r of
+                Void -> do
+                    error ""
+                _ -> do
+                    let st = modifyTable popTable st
+                    return ( modifyTable (pushTable (Context.ExprTable r Context.Dynamic Context.Nein)) st )
+        _ -> do
+            error ""
+                
+
+
+
 
 anaExpr (Out.Bracket e) = anaExpr e
 
---anaExpr (Out.ExpFcall f) = do
---	anaFuncion f
+
 
