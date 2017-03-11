6a163a8cea830fbb833770efa6591626547f53f9
Christian <christianol_01@hotmail.com>
2017-03-10 22:18:38 -0400

    Clean Instructions

---
+++ Instrucciones.hs
@@ -0,0 +1,13 @@
+module Instrucciones where
+import qualified Lexer as Lexer
+import Stdout as Out
+import qualified Grammar 
+import Data.Map as M 
+import Control.Monad.RWS
+import Context as Context
+import Control.Exception
+import Expresiones
+
+
+
+
